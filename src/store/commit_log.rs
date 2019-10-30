e71c833506787a6f785b0028b2682acfb4661fed
Besok <zhguchev@gmail.com>
2019-10-30 22:04:06 +0000

    add

--- src/store/commit_log.rs
+++ src/store/commit_log.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 static INDEX_FILE_NAME: &str = "commit_log.idx";
 
 struct IndexFile {
@@ -8,21 +10,64 @@ pub struct Index {
     val: u32
 }
 
+#[derive(Debug, Clone)]
+struct IndexError(&'static str);
+
 impl Index {
-    fn from(bytes: &[u8;4]) -> Index {
-        let val =  u32::from_be_bytes(*bytes);
+    fn to_bytes_array(idx_array: &Vec<Index>) -> Box<[u8]> {
+        if idx_array.is_empty() {
+            return Box::new([]);
+        }
+        let x = idx_array
+            .iter()
+            .flat_map(|idx| idx.to_bytes())
+            .collect();
+
+            return Box::new([]);
+    }
+
+    fn from_bytes_array(bytes: &[u8]) -> Result<std::vec::Vec<Index>, IndexError> {
+        if (bytes.len() % 4) != 0 {
+            return Err(IndexError("should divide by 4"));
+        }
+
+        Ok(
+            bytes
+                .chunks(4)
+                .map(|ch| Index::from_bytes(ch))
+                .collect()
+        )
+    }
+
+    fn convert(bytes: &[u8]) -> &[u8; 4] {
+        bytes.try_into().expect("expected an array with 4 bytes")
+    }
+
+    fn from_bytes(bytes: &[u8]) -> Index {
+        let val = u32::from_be_bytes(*Index::convert(bytes));
         Index { val }
     }
 
-    fn to(&self) -> [u8; 4] {
+    fn to_bytes(&self) -> [u8; 4] {
         self.val.to_be_bytes()
     }
 }
 
-impl IndexFile {
-    pub fn read(&self) -> Vec<Index> {
-        vec![]
+
+#[cfg(test)]
+mod tests {
+    use crate::store::commit_log::Index;
+
+    #[test]
+    fn index_test() {
+        let idx = Index { val: 1000_000_000 };
+
+        let bts = &idx.to_bytes();
+        let idx = Index::from_bytes(bts);
+
+        assert_eq!(idx.val, 1000_000_000);
+
+
+        let result = Index::from_bytes_array(bts);
     }
 }
-
-
